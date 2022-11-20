023f2ddf38c166ab969a64cf8872772b7348c52c
Jose <jose0797@gmail.com>
2022-11-19 19:38:21 -0500

    Backing up configs

--- kubernetes/network-config.sh
+++ kubernetes/network-config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Get network config.
-wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
 # Replace host name.
 sed -i 's/vxlan/host-gw/' kube-flannel.yml
